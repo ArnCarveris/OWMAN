33f2044fc459e846854e58487ff3cf811ff3dd30
ArnCarver <arncis96@gmail.com>
2018-07-21 00:58:59 +0300

    position: moved serialization implementation to inline file

--- src/position.hpp
+++ src/position.hpp
@@ -36,27 +36,7 @@ public:
 	Position operator-()const;
 
     template<typename Archive>
-    void serialize(Archive &archive, std::uint32_t const version) {
-
-        if (version > 0)
-        {
-            archive(cereal::make_nvp("cell", cell));
-            archive(cereal::make_nvp("offset", offset));
-        }
-        else
-        {
-            try {
-                archive(cereal::make_nvp(xmlstr::cell_x, cell.x));
-                archive(cereal::make_nvp(xmlstr::cell_y, cell.y));
-            }
-            catch (const std::exception e) { (void)e; }
-            
-            archive(cereal::make_nvp(xmlstr::x, offset.x));
-            archive(cereal::make_nvp(xmlstr::y, offset.y));
-        }
-    }
+    void serialize(Archive &archive);
 };
-
-CEREAL_CLASS_VERSION(Position, 1);
 
 #endif
---
+++ src/position.inl
@@ -0,0 +1,18 @@
+
+#include <cereal/cereal.hpp>
+#include "util/xmlstr.hpp"
+
+template<typename Archive>
+void Position::serialize(Archive &archive) {
+
+    try {
+        archive(cereal::make_nvp(xmlstr::cell_x, cell.x));
+        archive(cereal::make_nvp(xmlstr::cell_y, cell.y));
+    }
+    catch (const std::exception e) {
+        cell = Vec2i(0, 0); (void)e;
+    }
+
+    archive(cereal::make_nvp(xmlstr::x, offset.x));
+    archive(cereal::make_nvp(xmlstr::y, offset.y));
+}
